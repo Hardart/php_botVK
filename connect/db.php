770b3bf2e6462febb746bb5bb6109f9d57429f2c
Hardart <kaino@bk.ru>
2021-04-07 13:31:45 +0300

    del config files

--- connect/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-	$dbh = new PDO('mysql:dbname=h911249946_test; host=h911249946.mysql;charset=utf8', 'h911249946_hard', 'qaZ134679'); // подключаемся
-	$dbh->exec('SET CHARACTER SET utf8');
-} catch (PDOException $e) {
-	echo 'Подключение не удалось: ' . $e->getMessage();
-}
